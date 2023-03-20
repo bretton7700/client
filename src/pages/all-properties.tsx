8848bf7351ef508890b0190d687d59d386593885
gitonga bretton <gitongabretton@gmail.com>
2023-03-20 18:41:23 +0300

    all properties

--- src/pages/all-properties.tsx
+++ src/pages/all-properties.tsx
@@ -1,11 +1,19 @@
 import { Add } from '@mui/icons-material';
-import { useList } from '@pankod/refine-core';
+import { useTable } from '@pankod/refine-core';
 import {Box, Stack, Typography } from '@pankod/refine-mui';
 import { useNavigate } from '@pankod/refine-react-router-v6';
 
 import { PropertyCard, CustomButton } from 'components';
 const AllProperties = () => {
   const navigate = useNavigate();
+  const {
+    tableQueryResult: { data, isLoading, isError }
+  } = useTable();
+
+  const allProperties = data?.data ?? [];
+
+  if(isLoading) return <Typography>Loading....</Typography>
+  if(isError) return <Typography>Error....</Typography>
   return (
     <Box>
       <Stack direction="row" justifyContent="space-between" alignItems="center">
@@ -19,6 +27,22 @@ const AllProperties = () => {
 
 
       </Stack>
+
+      <Box mt="20px"  sx={{ display: 'flex', flexWrap: 'wrap', gap: 3}}>
+
+        {allProperties.map((property) => (
+          <PropertyCard
+          key={property._id}
+          id={property._id}
+          title={property.title}
+          price={property.price}
+          location={property.location}
+          photo={property.photo}
+        
+           />
+        ))}
+
+      </Box>
     </Box>
   )
 }
